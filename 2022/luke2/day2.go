9395ea84e69f0e01a6a291355c2774c075cea531
Odin Bjørnebo <odinbjoernebo@gmail.com>
2022-12-09 12:18:13 +0100

    part 2, awful but works.Would have needed refactor

--- 2022/luke2/day2.go
+++ 2022/luke2/day2.go
@@ -17,13 +17,16 @@ func main() {
 	f, _ := os.Open("luke2/input.txt")
 	defer f.Close()
 	scanner := bufio.NewScanner(f)
-	totalScore := 0
+	totalScore, totalScoreDay2 := 0, 0
 	for scanner.Scan() {
 		round := strings.Split(scanner.Text(), " ")
-		r := Round{rockPaperScissors(round[0]), rockPaperScissors(round[1]), 0}
+		r := Round{rockPaperScissors(round[0]), rockPaperScissors(round[1]), 0}                                     //This looks
+		r2 := Round{rockPaperScissors(round[0]), howToPlay(rockPaperScissors(round[0]), methodToPlay(round[1])), 0} //Awful
 		totalScore += calculateScore(r)
+		totalScoreDay2 += calculateScore(r2)
 	}
-	fmt.Println(totalScore)
+	fmt.Println("Part 1", totalScore)
+	fmt.Println("Part 2", totalScoreDay2)
 }
 
 func calculateScore(r Round) int {
@@ -65,6 +68,32 @@ func rockPaperScissors(s string) string {
 	case "B", "Y":
 		return "Paper"
 	case "C", "Z":
+		return "Scissor"
+	default:
+		return ""
+	}
+}
+
+func methodToPlay(s string) string {
+	switch s {
+	case "X":
+		return "Lose"
+	case "Y":
+		return "Tie"
+	case "Z":
+		return "Win"
+	default:
+		return ""
+	}
+}
+
+func howToPlay(inp string, method string) string {
+	switch {
+	case inp == "Rock" && method == "Win", inp == "Scissor" && method == "Lose", inp == "Paper" && method == "Tie":
+		return "Paper"
+	case inp == "Rock" && method == "Tie", inp == "Scissor" && method == "Win", inp == "Paper" && method == "Lose":
+		return "Rock"
+	case inp == "Rock" && method == "Lose", inp == "Scissor" && method == "Tie", inp == "Paper" && method == "Win":
 		return "Scissor"
 	default:
 		return ""
