c500c260cec8c7df0cb0d7291f81b345e87b323a
Odin Bjørnebo <odinbjoernebo@gmail.com>
2023-12-05 19:22:03 +0100

    new approach, solved p2 and overhauled p1

--- 2023/luke_5/main.go
+++ 2023/luke_5/main.go
@@ -6,141 +6,168 @@ import (
 	"os"
 	"strconv"
 	"strings"
-)
 
-type Category int64
-
-const (
-	Seed Category = iota
-	Soil
-	Fertilizer
-	Water
-	Light
-	Temperature
-	Humidity
-	Location
+	"github.com/samber/lo"
 )
 
 var (
-	categoryMap = map[string]Category{
-		"seed":        Seed,
-		"soil":        Soil,
-		"fertilizer":  Fertilizer,
-		"water":       Water,
-		"light":       Light,
-		"temperature": Temperature,
-		"humidity":    Humidity,
-		"location":    Location,
-	}
+	seedToSoil = []ConvMap{}
+	soilToFert = []ConvMap{}
+	fertToWtr  = []ConvMap{}
+	wtrToLght  = []ConvMap{}
+	lghtToTmp  = []ConvMap{}
+	tmpToHum   = []ConvMap{}
+	humToLoc   = []ConvMap{}
 )
 
-func (c Category) String() string {
-	return [...]string{"seed", "soil", "fertilizer", "water", "light", "temperature", "humidity", "location"}[c]
+type ConvMap struct {
+	src int
+	dst int
+	rng int
+}
+
+func fromLine(line string) ConvMap {
+	cMap := &ConvMap{}
+
+	split := lo.Map(strings.Split(line, " "), func(x string, _ int) int {
+		num, err := strconv.Atoi(x)
+		if err != nil {
+			panic(err)
+		}
+		return num
+	})
+
+	cMap.src = split[1]
+	cMap.dst = split[0]
+	cMap.rng = split[2]
+
+	return *cMap
+}
+
+func Solution1() error {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	scnr := bufio.NewScanner(file)
+
+	scnr.Scan()
+	line := scnr.Text()
+	seeds := lo.Map(strings.Split(strings.Trim(strings.Split(line, ":")[1], " "), " "), func(x string, _ int) int {
+		num, err := strconv.Atoi(x)
+		if err != nil {
+			panic(err)
+		}
+		return num
+	})
+
+	readMaps(scnr, &seedToSoil)
+	readMaps(scnr, &soilToFert)
+	readMaps(scnr, &fertToWtr)
+	readMaps(scnr, &wtrToLght)
+	readMaps(scnr, &lghtToTmp)
+	readMaps(scnr, &tmpToHum)
+	readMaps(scnr, &humToLoc)
+
+	locs := make([]int, 0)
+
+	for _, seed := range seeds {
+		soil := srcToDst(seed, seedToSoil)
+		fert := srcToDst(soil, soilToFert)
+		wtr := srcToDst(fert, fertToWtr)
+		lght := srcToDst(wtr, wtrToLght)
+		tmp := srcToDst(lght, lghtToTmp)
+		hum := srcToDst(tmp, tmpToHum)
+		loc := srcToDst(hum, humToLoc)
+		locs = append(locs, loc)
+	}
+
+	fmt.Println(lo.Min(locs))
+	return nil
+}
+func Solution2() error {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	scnr := bufio.NewScanner(file)
+
+	scnr.Scan()
+	line := scnr.Text()
+	seeds := lo.Map(strings.Split(strings.Trim(strings.Split(line, ":")[1], " "), " "), func(x string, _ int) int {
+		num, err := strconv.Atoi(x)
+		if err != nil {
+			panic(err)
+		}
+		return num
+	})
+
+	readMaps(scnr, &seedToSoil)
+	readMaps(scnr, &soilToFert)
+	readMaps(scnr, &fertToWtr)
+	readMaps(scnr, &wtrToLght)
+	readMaps(scnr, &lghtToTmp)
+	readMaps(scnr, &tmpToHum)
+	readMaps(scnr, &humToLoc)
+
+	locs := make([]int, 0)
+
+	for i := 0; i < len(seeds); i += 2 {
+		for seed := seeds[i]; seed < seeds[i]+seeds[i+1]; seed++ {
+			soil := srcToDst(seed, seedToSoil)
+			fert := srcToDst(soil, soilToFert)
+			wtr := srcToDst(fert, fertToWtr)
+			lght := srcToDst(wtr, wtrToLght)
+			tmp := srcToDst(lght, lghtToTmp)
+			hum := srcToDst(tmp, tmpToHum)
+			loc := srcToDst(hum, humToLoc)
+			locs = append(locs, loc)
+		}
+	}
+
+	fmt.Println(lo.Min(locs))
+	return nil
+}
+
+func srcToDst(val int, maps []ConvMap) int {
+	for _, m := range maps {
+		if val >= m.src && val < m.src+m.rng {
+			return m.dst + (val - m.src)
+		}
+	}
+	return val
+}
+
+func readMaps(scnr *bufio.Scanner, maps *[]ConvMap) {
+	var line string
+	for scnr.Scan() {
+		line := scnr.Text()
+		if strings.Contains(line, ":") {
+			break
+		}
+	}
+
+	for scnr.Scan() {
+		line = scnr.Text()
+		if line == "" {
+			break
+		}
+
+		*maps = append(*maps, fromLine(line))
+	}
 }
 
 func main() {
-	f, err := os.Open("input.txt")
-	if err != nil {
-		panic(err)
+	//s := time.Now()
+	var e error
+	e = Solution1() //322500873
+	e = Solution2() //108956227
+	//fmt.Println(time.Since(s))
+	if e != nil {
+		panic(e)
 	}
-	defer f.Close()
-	scnr := bufio.NewScanner(f)
-	scnr.Scan()
-
-	seeds := strings.Split(strings.Trim(strings.Split(scnr.Text(), ":")[1], " "), " ")
-	intSeeds := make([]int, len(seeds))
-	for ind, s := range seeds {
-		i, _ := strconv.Atoi(s)
-		intSeeds[ind] = i
-	}
-
-	scnr.Scan()
-	ind := 0
-	maps := make([]*catMap, 7)
-	var from Category
-	var to Category
-	for scnr.Scan() {
-		t := scnr.Text()
-		switch {
-		case len(strings.Split(t, ":")) > 1:
-			from = categoryMap[strings.Split(t, "-")[0]]
-			to = categoryMap[strings.Split(strings.Split(t, "-")[2], " ")[0]]
-			maps[ind] = &catMap{
-				from:    from,
-				to:      to,
-				fromMap: map[int]int{},
-				toMap:   map[int]int{},
-				linkMap: map[int]int{},
-			}
-		case len(strings.Split(t, " ")) > 2:
-			i := getInts(t)
-			maps[ind].fromMap[i[1]] = i[2]
-			maps[ind].toMap[i[0]] = i[2]
-			maps[ind].linkMap[i[1]] = i[0]
-		default:
-			ind += 1
-		}
-	}
-
-	for _, m := range maps {
-		for ind, seed := range intSeeds {
-			intSeeds[ind] = toFrom(seed, m.fromMap, m.toMap, m.linkMap)
-		}
-	}
-	fmt.Println("Task1: Smallest Location ", findMinElement(intSeeds))
-}
-
-type catMap struct {
-	from    Category
-	to      Category
-	fromMap map[int]int
-	toMap   map[int]int
-	linkMap map[int]int
-}
-
-func isInRange(inp int, start int, r int) bool {
-	return (inp >= start && inp < start+r)
-}
-
-func inMap(inp int, m map[int]int) (int, bool) {
-	for k, v := range m {
-		if isInRange(inp, k, v) {
-			return k, true
-		}
-	}
-	return 0, false
-}
-
-func toFrom(inp int, from, to, link map[int]int) int {
-	kInt, b := inMap(inp, from)
-	if b {
-		toKey := link[kInt]
-		if toKey > kInt {
-			return inp + (toKey - kInt)
-		} else {
-			return inp - (kInt - toKey)
-		}
-	}
-	return inp
-}
-
-func getInts(s string) []int {
-	stringNum := strings.Split(s, " ")
-	var n = make([]int, 3)
-	dest, _ := strconv.Atoi(stringNum[0])
-	from, _ := strconv.Atoi(stringNum[1])
-	r, _ := strconv.Atoi(stringNum[2])
-	n[0], n[1], n[2] = dest, from, r
-	return n
-}
-
-func findMinElement(arr []int) int {
-	min_num := arr[0]
-
-	for i := 0; i < len(arr); i++ {
-		if arr[i] < min_num {
-			min_num = arr[i]
-		}
-	}
-	return min_num
 }
