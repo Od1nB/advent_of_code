4ad6422c9d8f4b384bb9ff1026e1dad319e2c299
Odin Bjørnebo <odinbjoernebo@gmail.com>
2023-12-09 13:51:04 +0100

    part 1 and 2

--- 2023/luke_9/main.go
+++ 2023/luke_9/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 	"strings"
@@ -16,7 +17,7 @@ type Entry struct {
 }
 
 func main() {
-	f, err := os.Open("example.txt")
+	f, err := os.Open("input.txt")
 	if err != nil {
 		panic(err)
 	}
@@ -33,6 +34,68 @@ func main() {
 				return n
 			}),
 		})
+		fillExtras(&entries[ind])
 		ind++
 	}
+	fmt.Println("task1: ", appendHistory(entries))  //2175229206
+	fmt.Println("task2: ", prePendHistory(entries)) //942
+}
+
+func appendHistory(entries []Entry) int {
+	score := 0
+	for _, v := range entries {
+		for i := len(v.extras) - 1; i >= 0; i-- {
+			if isAllZeroes(v.extras[i]) {
+				v.extras[i] = append(v.extras[i], 0)
+			} else {
+				v.extras[i] = append(v.extras[i], v.extras[i][len(v.extras[i])-1]+v.extras[i+1][len(v.extras[i])-1])
+			}
+		}
+		num := v.history[len(v.history)-1] + v.extras[0][len(v.extras[0])-1]
+		score += num
+	}
+
+	return score
+}
+
+func prePendHistory(entries []Entry) int {
+	score := 0
+	for _, v := range entries {
+		//fmt.Print(v.extras)
+		for i := len(v.extras) - 1; i >= 0; i-- {
+			if isAllZeroes(v.extras[i]) {
+				v.extras[i] = append([]int{0}, v.extras[i]...)
+			} else {
+				v.extras[i] = append([]int{v.extras[i][0] - v.extras[i+1][0]}, v.extras[i]...)
+			}
+		}
+		num := v.history[0] - v.extras[0][0]
+		score += num
+
+	} //task2 942
+	return score
+}
+func fillExtras(e *Entry) {
+	intrmdt := make([]int, len(e.history)-1)
+	for i := 1; i < len(e.history); i++ {
+		intrmdt[i-1] = e.history[i] - (e.history[i-1])
+	}
+	e.extras = append(e.extras, intrmdt)
+	for !isAllZeroes(intrmdt) {
+		loopintrmdt := make([]int, len(intrmdt)-1)
+		for i := 1; i < len(intrmdt); i++ {
+			loopintrmdt[i-1] = intrmdt[i] - (intrmdt[i-1])
+		}
+		e.extras = append(e.extras, loopintrmdt)
+		intrmdt = loopintrmdt
+	}
+}
+
+func isAllZeroes(arr []int) bool {
+	for _, num := range arr {
+		if num != 0 {
+			return false
+		}
+	}
+	return true
 }
