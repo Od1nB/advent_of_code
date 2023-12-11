c9632e76001563b108daf054d29e0fbe0ec02bdb
Odin Bjørnebo <odinbjoernebo@gmail.com>
2023-12-11 15:52:57 +0100

    luke 11, part 1 did cartesian calculation

--- 2023/luke_11/main.go
+++ 2023/luke_11/main.go
@@ -4,7 +4,12 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
+
+type Coord struct {
+	x, y int
+}
 
 func main() {
 	f, err := os.Open("input.txt")
@@ -13,13 +18,85 @@ func main() {
 	}
 	defer f.Close()
 	scnr := bufio.NewScanner(f)
-	task1, task2 := 0, 0
-
+	universe := []string{}
 	for scnr.Scan() {
 		t := scnr.Text()
-		fmt.Println(t)
+		universe = append(universe, t)
 	}
 
-	fmt.Println("task1: ", task1)
-	fmt.Println("task2: ", task2)
+	emptyRows, emptyColumns := getEmptyRows(universe), getEmptyColumns(universe)
+
+	galaxies := getGalaxies(universe, emptyRows, emptyColumns)
+
+	fmt.Println("task1: ", calcScore(galaxies))
+	fmt.Println("task2: ", 0)
+}
+
+func calcScore(galaxies map[int]Coord) int {
+	acc := 0
+	for i := range galaxies {
+		for j := i + 1; j < len(galaxies); j++ {
+			acc += abs(galaxies[i].x-galaxies[j].x) + abs(galaxies[i].y-galaxies[j].y)
+		}
+	}
+	return acc
+}
+
+func getGalaxies(universe []string, emptyRows, emptyColumns map[int]bool) map[int]Coord {
+	galaxies := map[int]Coord{}
+	n := 0
+	for i := 0; i < len(universe); i++ {
+		for j := 0; j < len(universe[i]); j++ {
+			if string(universe[i][j]) == "#" {
+				nx, ny := 0, 0
+				for k := 0; k < i; k++ {
+					if emptyRows[k] {
+						ny++
+					}
+				}
+				for k := 0; k < j; k++ {
+					if emptyColumns[k] {
+						nx++
+					}
+				}
+				galaxies[n] = Coord{x: i + ny, y: j + nx}
+				n++
+			}
+		}
+	}
+	return galaxies
+}
+
+func getEmptyRows(lines []string) map[int]bool {
+	rowMap := map[int]bool{}
+	for i, l := range lines {
+		if strings.Contains(l, "#") {
+			continue
+		}
+		rowMap[i] = true
+	}
+	return rowMap
+}
+
+func getEmptyColumns(lines []string) map[int]bool {
+	columnMap := map[int]bool{}
+	for i := range lines[0] {
+		l := ""
+		for j := range lines {
+			l += string(lines[j][i])
+		}
+		if strings.Contains(l, "#") {
+			continue
+		}
+		columnMap[i] = true
+	}
+	return columnMap
+
+}
+
+func abs(x int) int {
+	if x > 0 {
+		return x
+	}
+	return -x
 }
