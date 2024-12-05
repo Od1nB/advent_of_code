e97e242c7fd6ef29afb5e2202c1585a9d11c5c8f
Odin Bjørnebo <odinbjoernebo@gmail.com>
2024-12-05 12:08:46 +0100

    luke 5 intersection check

--- 2024/luke_5/go.mod
+++ 2024/luke_5/go.mod
@@ -1,3 +1,8 @@
 module luke_5
 
 go 1.23.3
+
+require (
+	github.com/samber/lo v1.47.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+)
--- 2024/luke_5/main.go
+++ 2024/luke_5/main.go
@@ -4,6 +4,11 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
+	"strconv"
+	"strings"
+
+	"github.com/samber/lo"
 )
 
 func main() {
@@ -19,12 +24,99 @@ func main() {
 	defer f.Close()
 	scnr := bufio.NewScanner(f)
 	task1, task2 := 0, 0
+	var afterNewline bool
+	var correct, incorrect [][]int
+	orders := map[int][]int{}
 
 	for scnr.Scan() {
 		t := scnr.Text()
-		fmt.Println(t)
+		if t == "" {
+			afterNewline = true
+			continue
+		}
+
+		if !afterNewline {
+			s1, s2, _ := strings.Cut(t, "|")
+			n1, err := strconv.Atoi(s1)
+			n2, err := strconv.Atoi(s2)
+			if err != nil {
+				panic(err)
+			}
+			orders[n1] = append(orders[n1], n2)
+		}
+
+		if afterNewline {
+			nums := lo.Map(strings.Split(t, ","), func(s string, _ int) int {
+				num, err := strconv.Atoi(s)
+				if err != nil {
+					panic(err)
+				}
+				return num
+			})
+			if correctOrder(orders, nums) {
+				correct = append(correct, nums)
+				continue
+			}
+			incorrect = append(incorrect, nums)
+		}
 	}
 
-	fmt.Println("task1: ", task1)
-	fmt.Println("task2: ", task2)
+	for _, list := range correct {
+		calcScore(list, &task1)
+	}
+	for _, list := range incorrect {
+		calcScore(repair(orders, list), &task2)
+	}
+
+	fmt.Println("task1: ", task1) // 7365
+	fmt.Println("task2: ", task2) // 5770
+}
+
+func correctOrder(orders map[int][]int, update []int) bool {
+	seen := []int{}
+	for _, num := range update {
+		if os, ok := orders[num]; ok {
+			if wrong := intersection(os, append(seen, num)); len(wrong) != 0 {
+				return false
+			}
+			seen = append(seen, num)
+
+		}
+	}
+	return true
+}
+
+func calcScore(nums []int, res *int) {
+	*res += nums[len(nums)/2]
+}
+
+func repair(orders map[int][]int, update []int) []int {
+	fixed := make([]int, 0, len(update))
+	for ind, num := range update {
+		fixed = append(fixed, num)
+		if wrong := intersection(orders[num], fixed[:ind]); len(wrong) != 0 {
+			fixed = slices.DeleteFunc(fixed, func(i int) bool {
+				return slices.Contains(wrong, i)
+			})
+			fixed = append(fixed, wrong...)
+		}
+	}
+
+	return fixed
+}
+
+func intersection(a, b []int) []int {
+	m := make(map[int]bool, 0)
+	set := make([]int, 0)
+	for _, num := range a {
+		m[num] = true
+	}
+
+	for _, num := range b {
+		if m[num] {
+			set = append(set, num)
+		}
+	}
+
+	return set
 }
